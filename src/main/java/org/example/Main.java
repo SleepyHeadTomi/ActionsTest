b86755080ad7ca8ac0ecd71ab3237662848f715c
SleepyHeadTomi <152853332+SleepyHeadTomi@users.noreply.github.com>
2023-12-05 09:33:15 +0100

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Mitt favoritrecept");
+        System.out.println("1. Häll upp 6 cl av en fin bourbon i ett glas");
+        System.out.println("2. Tillsätt några droppar ljummet vatten");
+        System.out.println("3. Avnjut drycken!");
+    }
+}
